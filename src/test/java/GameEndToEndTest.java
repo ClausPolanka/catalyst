1584469120b620619eaa64682771c21b2e21c736
Claus Polanka <claus.polanka@chello.at>
2011-10-21 16:21:40 +0200

    First commit

---
+++ src/test/java/GameEndToEndTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GameEndToEndTest {
+    @Test
+    public void testName() throws Exception {
+        assertEquals(true, false);
+    }
+}
